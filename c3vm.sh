52d04c9f774e6cfff4835599a34d5e947fa62c5e
Bram Windey <windey.bram@gmail.com>
2025-07-05 12:10:42 +0200

    Implement print_long_help and print_short_help

--- c3vm.sh
+++ c3vm.sh
@@ -1,76 +1,105 @@
 #!/bin/bash
 
-# Welcome to my highly advanced c3c version manager.
-# This is a bash script that can install and manage versions of the c3c compiler.
-# It can grab releases from Github or compile from scratch
-#
-# Usage:
-# 	c3vm <command> <flags>
-#
-# 	Commands:
-# 		- list                  List (installed) compilers
-# 		- install [<version>]   Install specified version, or latest when
-# 		                        version is omitted
-# 		- remove <version>      Remove specified version (regex match with grep)
-# 		- use <version>         Use the specified version for a single command
-#
-# 	Flags:
-# 	- Global:
-# 		--version, -V           Print version of this script
-# 		--verbose, -v           Verbose logging of all subcommands
-# 		--help, -hh             Print this long help
-# 		-h                      Print short help
-#
-# 	- List command:
-# 		--installed, -i         List installed compilers (default)
-# 		--enabled, -e           List only the single enabled compiler
-# 		--all, -a               List all available compilers (from Github)
-# 		--release               Filter on release version
-# 		--debug                 Filter on debug versions
-#
-# 	- Install command:
-# 		--from-source [<hash>]  Compile from source. Defaults to latest commit
-# 	                            on the default branch, but can be tweaked by
-# 	                            specifying the hash of the commit or with --branch
-# 		--branch <branch>       Specify branch for --edge or --commit
-# 		--remote <url>          Use a different git-remote,
-# 		                        default https://github.com/c3lang/c3c
-# 		--debug                 Install the debug version
-# 		--dont-enable           Do not enable the new version (keep old one active)
-#
-# 	- Remove command:
-# 		--interactive, -I       Prompt before removing a version
-# 		--no-regex, -F          Interpret <version> as fixed-string instead of
-# 		                        regex pattern
-# 		--inactive              Remove all installed compilers except for the
-# 		                        currently enabled compiler
-#
-# 	- Use command:
-# 		--install               Install the version first if it wasn't already
-#                               (behind this flag you can add the "install" flags)
-# 		--session               Set an environment variable to use the specified
-# 		                        version for the rest of your shell session
-#
-#
-# 	Additional info:
-#		The compilers are stored under $XDG_DATA_HOME/c3vm/, where $XDG_DATA_HOME
-#		defaults to $HOME/.local/share/.
-#
-#		Building from git will happen inside $XDG_CACHE_HOME/c3vm/, where
-#		$XDG_CACHE_HOME defaults to $HOME/.cache/.
-#
-#		Versions are according to the tag on github. You can request a debug-
-#		build either with '--debug' or by adding '-debug' to the version,
-#		f.e. '0.7.3-debug'.
-#
-#		The current enabled version is symlinked (`ln -s`) to $HOME/.local/bin.
-#
-#		There are quite some other configurable things you can tweak by
-#		tweaking some bash variable below this explanation. Or just straight up
-#		tweak the source code.
+function print_long_help() {
+	cat << 'LONG_HELP'
+Welcome to my highly advanced c3c version manager.
+This is a bash script that can install and manage versions of the c3c compiler.
+It can grab releases from Github or compile from scratch
+
+ Usage:
+    c3vm [<command>] [<flags>] [<args>]
+
+ Commands:
+    - list                  List (installed) compilers
+    - install [<version>]   Install specified version, or latest when
+                            version is omitted
+    - remove <version>      Remove specified version (regex match with grep)
+    - use <version> [-- <args>]
+                            Use the specified version for a single command
+                            and pass the <args> to the compiler
+
+ Flags:
+ - Global:
+    --version, -V           Print version of this script
+    --verbose, -v           Log all info
+    --quiet, -q             Suppress all info (not errors)
+    --help, -hh             Print this long help
+    -h                      Print short help
+
+ - List command:
+    --installed, -i         List installed compilers (default)
+    --enabled, -e           List only the single enabled compiler
+    --all, -a               List all available compilers (from Github)
+    --release               Filter on release version
+    --debug                 Filter on debug versions
+
+ - Install command:
+    --from-source [<hash>]  Compile from source. Defaults to latest commit
+                            on the default branch, but can be tweaked by
+                            specifying the hash of the commit or with --branch
+    --branch <branch>       Specify branch for --edge or --commit
+    --remote <url>          Use a different git-remote,
+                            default https://github.com/c3lang/c3c
+    --debug                 Install the debug version
+    --dont-enable           Do not enable the new version (keep old one active)
+
+ - Remove command:
+    --interactive, -I       Prompt before removing a version
+    --no-regex, -F          Interpret <version> as fixed-string instead of
+                            regex pattern
+    --inactive              Remove all installed compilers except for the
+                            currently enabled compiler
+
+ - Use command:
+    --install               Install the version first if it wasn't already
+                            (behind this flag you can add the "install" flags)
+    --session               Output the exports to switch current compiler
+                            version in your shell session.
+                            Should be used as `eval "$(c3vm use --session <version>)"`.
 
 
-version="0.7.3" # Following the c3c release cycle a bit. Seems fun.
+ Additional info:
+    The compilers are stored under $XDG_DATA_HOME/c3vm/, where $XDG_DATA_HOME
+    defaults to $HOME/.local/share/.
+
+    Building from git will happen inside $XDG_CACHE_HOME/c3vm/, where
+    $XDG_CACHE_HOME defaults to $HOME/.cache/.
+
+    Versions are according to the tag on github. You can request a debug-
+    build either with '--debug' or by adding '-debug' to the version,
+    f.e. '0.7.3-debug'.
+
+    The current enabled version is symlinked (`ln -s`) to $HOME/.local/bin.
+
+    There are quite some other configurable things you can tweak by
+    tweaking some bash variable below this explanation. Or just straight up
+    tweak the source code.
+
+
+ Exit codes:
+    0 - OK
+    1 - Required directories missing and not able to create them
+    2 - Multiple subcommands found
+    3 - Flag misses (correct) argument
+    4 - Flag is used without its subcommand
+    5 - Flag is used with wrong subcommand
+    6 - Contradicting flags
+    7 - Unknow argument/flag
+    8 - Version did not match version-regex
+LONG_HELP
+	exit
+}
+
+function print_short_help() {
+	cat << 'SHORT_HELP'
+Usage: c3vm [<command>] [<flags>] [<args>]
+Commands: list, install [<version>], remove <version>, use <version>
+Global flags: --version, --verbose, --quiet, --help
+SHORT_HELP
+}
+
+# Tweakable variables
+VERSION="0.7.3" # Following the c3c release cycle a bit. Seems fun.
 
 data_home="${XDG_DATA_HOME:-$HOME/.local/share}/c3vm"
 cache_home="${XDG_CACHE_HOME:-$HOME/.cache}/c3vm"
